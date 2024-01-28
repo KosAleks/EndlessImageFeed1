45f8b7defd54e689233667a3cf8fe800cdd7a9ca
Aleksandra <aleksandra.kosnireva@yandex.ru>
2024-01-28 19:24:49 +0500

    LaunchScreen completed. The image feed screen has been added and laid out according to layouts.

--- EndlessImageFeed/ImageList/ImageListCell.swift
+++ EndlessImageFeed/ImageList/ImageListCell.swift
@@ -10,7 +10,6 @@ import UIKit
 
 final class ImageListCell: UITableViewCell {
     static let reuseIdentifier = "ImageListCell"
-    
     @IBOutlet var likeButtonActive: UIButton!
     @IBOutlet var dataLabel: UILabel!
     @IBOutlet var imageCell: UIImageView!
